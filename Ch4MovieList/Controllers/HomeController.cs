ddd8167791d027c817d89a0e0280c8b4780273b6
Gage <gdecker@dmacc.edu>
2021-02-11 13:57:31 -0600

    Add project files.

---
+++ Ch4MovieList/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ch4MovieList.Models;
+
+namespace Ch4MovieList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MovieContext context { get; set; }
+
+        public HomeController(MovieContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            var movies = context.Movies.Include(m => m.Genre).OrderBy(m => m.Name).ToList();
+            return View(movies);
+        }
+    }
+}
